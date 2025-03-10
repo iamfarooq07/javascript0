ecfd686cda4ce086b58d0189a994ab0bf0ee2401
Muhammad Farooq <fm6182218@gmail.com>
2025-03-10 20:54:28 +0500

    pras commit

---
+++ script-1.js
@@ -0,0 +1,46 @@
+// var empolyes = [
+//     {
+//         firstName: "Ahamed",
+//         designation: "Manager",
+//         salary: 55000,
+//         time: "10 to 5",
+//         present: true,
+//     },
+//     {
+//         firstName: "Maaz",
+//         designation: "computer operator",
+//         salary: 25000,
+//         time: "8 to 5",
+//         present: false,
+//     },
+//     {
+//         firstName: "Danish",
+//         designation: "Documents writer",
+//         salary: 40000,
+//         time: "9 to 5",
+//         present: true,
+//     },
+//     {
+//         firstName: "Nisar",
+//         designation: "offies Helper",
+//         salary: 20000,
+//         time: "8 to 5",
+//         present: false,
+//     },
+// ]
+// console.log(empolyes)
+
+// var sum = 0;
+
+// for (let i = 0; i < empolyes.length; i++) {
+//     // console.log(empolyes,length)
+//     sum = sum + empolyes[i].salary
+// }
+// console.log('Salarys : ' +sum)
+// var avg = sum / empolyes.length;
+// console.log("Avg : " + avg)
+// console.log("Name : " + empolyes[3].firstName)
+// console.log("Designation : " + empolyes[3].designation)
+// console.log("Salary : " + empolyes[3].salary)
+// console.log("Time : " + empolyes[3].time)
+// console.log('Present : ' + empolyes[3].present)
