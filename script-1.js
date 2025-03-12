be4aedb99217149a3683a69bf3ebbfc74febc1d3
Muhammad Farooq <fm6182218@gmail.com>
2025-03-12 20:02:18 +0500

    array pras

--- script-1.js
+++ script-1.js
@@ -1,88 +1,36 @@
-// var empolyes = [
-//     {
-//         firstName: "Ahamed",
-//         designation: "Manager",
-//         salary: 55000,
-//         time: "10 to 5",
-//         present: true,
-//     },
-//     {
-//         firstName: "Maaz",
-//         designation: "computer operator",
-//         salary: 25000,
-//         time: "8 to 5",
-//         present: false,
-//     },
-//     {
-//         firstName: "Danish",
-//         designation: "Documents writer",
-//         salary: 40000,
-//         time: "9 to 5",
-//         present: true,
-//     },
-//     {
-//         firstName: "Nisar",
-//         designation: "offies Helper",
-//         salary: 20000,
-//         time: "8 to 5",
-//         present: false,
-//     },
-// ]
-// console.log(empolyes)
 
-// var sum = 0;
 
-// for (let i = 0; i < empolyes.length; i++) {
-//     // console.log(empolyes,length)
-//     sum = sum + empolyes[i].salary
-// }
-// console.log('Salarys : ' +sum)
-// var avg = sum / empolyes.length;
-// console.log("Avg : " + avg)
-// console.log("Name : " + empolyes[3].firstName)
-// console.log("Designation : " + empolyes[3].designation)
-// console.log("Salary : " + empolyes[3].salary)
-// console.log("Time : " + empolyes[3].time)
-// console.log('Present : ' + empolyes[3].present)
+// //  -----------string-method------------//
 
-//--------------------------------------/
-
-//  -----------string-method------------//
- 
 // let b = "farooq";
-
 // console.log(b.charAt())
 // console.log(b.at(1))
 // console.log(b.toLowerCase())
 // console.log(b.toUpperCase())
 // console.log(b.length)
-// console.log(b.slice(0,3))
+// console.log(b.slice(0, 3))
 // console.log(b.slice(2))
-// console.log(b.replace("roo" , "123"))
-// console.log(b.concat("ali", " = " , "maaz" , "umer"))
+// console.log(b.replace("roo", "123"))
+// console.log(b.concat("ali", " = ", "maaz", "umer"))
 // console.log(b.indexOf())
-// console.log(b.toString)
+// console.log(b.toString("ali"))
+// console.log()
+// console.log()
 
-//------- / 
 
-// Write a JavaScript program that takes a number as input and checks whether it is positive, negative, or zero.
- 
-// var  num = prompt ("Enter Your number");
-
-// if (num > 0 && num < 10) {
-//     console.log("positive")
-// }else if (num < 0){
-//  console.log("nagetive")
-// }
-// else{
-//     console.log("Zero")
-// }
 
 //------/
 
-//------/
 //DOM=document object model -----/
 
-document.title="farooq is good";
-document.body.style.backgroundColor="green"
-console.log(document.body)
+// document.title = "farooq is good";
+// document.body.style.backgroundColor = "green"
+// console.log(document.body)
+
+//-----/
+
+let a1 = [1, 2, 3, 4,]
+let a2 = [5, 6, 7, 8,]
+let a3 = [9, 10, 11, 12,]
+
+a1.concat(a2, a3)
