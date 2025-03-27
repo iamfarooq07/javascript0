379d71bc078f9635990727de4f9d0cf392c44692
Muhammad Farooq <fm6182218@gmail.com>
2025-03-27 20:25:14 +0500

    first commit

---
+++ youtube/script-01.js
@@ -0,0 +1,47 @@
+//----------/
+
+// let farooq = document.getElementsByClassName ("boxs")
+// console.log(farooq)
+
+// farooq[2].style.backgroundColor = "red"
+
+// let umer=document.getElementsByClassName("box")
+// console.log(umer)
+// umer [2].style.backgroundColor = "blue"
+
+// document.getElementById("home").style.backgroundColor = "pink"
+
+// let boxes = document.getElementsByClassName("box-1")
+// console.log(boxes)
+
+// boxes [2].style .backgroundColor="yellow"
+
+// //-------/
+// Squre root qus------
+
+// var squre = prompt('Plase enter a number');
+
+// var a = Math.sqrt(squre);
+
+// console.log(a);
+
+//-----/
+
+// This is a dynamic number/
+
+// var a = prompt('Plesa Enter first number')
+// var b = prompt('Plesa Enter secend number')
+
+// var c = parseInt(a) + parseInt(b);
+// console.log("The Sum is "+ c);
+
+// ---------/
+
+//This is static number/
+
+var a = 3
+var b = 4
+
+var c = a + b;
+
+console.log("The Sum is "+ c)
